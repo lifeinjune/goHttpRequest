831fa06c7a806306286e10568527823a2243b594
Jun <geffrey90@hotmail.com>
2019-03-21 01:37:28 +0800

    added get request and comments

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
+func main() {
+	resp, err := http.Get("http://google.com") // http get request by using http.Get function
+	if err != nil {                            //if there is error
+		fmt.Println("Error:", err) //print error statement
+		os.Exit(1)                 //terminate the program
+	}
+	fmt.Println(resp) //print get response
 }
