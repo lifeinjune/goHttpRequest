7563ef6ea73b4819e7fe84b8578e9a419a327c99
Jun <geffrey90@hotmail.com>
2019-03-21 02:41:40 +0800

    change to simpler body extraction for request

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
@@ -14,9 +15,23 @@ func main() {
 	}
 	//fmt.Println(resp) //print get response
 
-	bs := make([]byte, 99999) // make empty byte slice with empty element of 99999 to begin with by using make function
-	resp.Body.Read(bs)
-	fmt.Println(string(bs))
+	// bs := make([]byte, 99999) // make empty byte slice with empty element of 99999 to begin with by using make function
+	// //becaseu reader function cannot resize the byte slice, slice initialize with many empty element to accomodate upcoming data stored inside without run out of space
+	// resp.Body.Read(bs)
+	// /*
+	// * Reader function accept slice of byte as argument
+	// * Body element in struct is type of readerCloser which mean as long as it meet the requirment of the interface anything can be in the Body as value
+	// * original data this case "Body" is element of the resp struct and type of the ReaderCloser interface so can access to Reader
+	// * with Body as source of Reader function, the byte slice we passed as argument will contain the Body part of the request because reader function have access to the original data because
+	// slice is reference type
+	// */
+	// fmt.Println(string(bs))
+
+	io.Copy(os.Stdout, resp.Body) //simpler form of print out body of http request
+	/*
+		io.Copy accept type Writer and Reader (writer as destination and reader as source) and copy the data from source to the destination
+		Writer is exactly oposit of the Reader it read the data from the program and interface (interpret) to many different source of output
+	*/
 }
 
 // interface like reader is interface for many other output with different types and change them into something common
